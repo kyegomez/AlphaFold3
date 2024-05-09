82418157a01af1475743a448df3f532e1eac6c81
Kye <kye@apacmediasolutions.com>
2024-05-09 16:48:50 -0400

    [CLEANUP]

--- diffusion_example.py
+++ diffusion_example.py
@@ -1,8 +1,8 @@
 import torch
-from alphafold3.diffusion import GeneticDiffusionModuleBlock
+from alphafold3.diffusion import GeneticDiffusion
 
 # Create an instance of the GeneticDiffusionModuleBlock
-model = GeneticDiffusionModuleBlock(channels=3, training=True)
+model = GeneticDiffusion(channels=3, training=True)
 
 # Generate random input coordinates
 input_coords = torch.randn(10, 100, 100, 3)
